4d2bed1e17e83f36c746685b2f9511466c121693
RitikPatel00 <patelritik0022@gmail.com>
2024-06-06 17:55:59 +0530

    jpa+spring boot

---
+++ spring-boot-jpa/src/main/java/com/wipro/aspect/UserAspect.java
@@ -0,0 +1,10 @@
+package com.wipro.aspect;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+@Aspect
+@Slf4j
+@Component
+public class UserAspect {
+	//TO DO
+}
